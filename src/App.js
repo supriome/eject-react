6a41cd2a2476519e542d89369b0b8e867e39ec6c
suprio <zhoupeng@fimvisual.com>
2020-08-03 17:06:44 +0800

    create react app tutorial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "@material-ui/core";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="primary">Hello World!</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
